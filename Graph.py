a6d8cb6da639e2929ac69fa1adcc2991fbb7a92a
Seaya Liu <seayaliu7@gmail.com>
2023-12-04 23:37:33 -0500

    more code

--- Graph.py
+++ Graph.py
@@ -37,10 +37,10 @@ class WeightedGraph(Graph):
     def add_node(self):
         self.adj[len(self.adj)] = []
 
-    def add_edge(self, node1, node2):
+    def add_edge(self, node1, node2, w):
         if node2 not in self.adj[node1]:
             self.adj[node1].append(node2)
-        self.weights[(node1, node2)] = 5 # calculate euclidian distance here
+        self.weights[(node1, node2)] = w # calculate euclidian distance here
 
     def get_num_of_nodes(self):
         return len(self.adj)
@@ -55,31 +55,6 @@ class WeightedGraph(Graph):
         if present:
             return self.weights[(node1, node2)]
 
-class HeuristicGraph(Graph):
-    heuristic = {}
-
-    def __init__(self, n):
-        self.adj = {}
-        for i in range(n):
-            self.adj[i] = []
-        
-    def get_adj_nodes(self, n):
-        return self.adj[n]
-
-    def add_node(self):
-        self.adj[len(self.adj)] = []
-
-    def add_edge(self, node1, node2):
-        if node1 not in self.adj[node2]:
-            self.adj[node1].append(node2)
-            self.adj[node2].append(node1)
-
-    def get_num_of_nodes(self):
-        return len(self.adj)
-    
-    def get_heuristic(self):
-        return heuristic
-
 def main():
     graphStructure = {}
     stationsFile = 'london_stations.csv'
@@ -87,10 +62,14 @@ def main():
     
     with open(stationsFile, 'r') as stations:
         stations_reader = csv.reader(stations)
-        next(stations_reader)  # Skip header
+        next(stations_reader) 
         for row in stations_reader:
             station_id, latitude, longitude = map(int, row)
-            graph[station_id] = {'latitude': latitude, 'longitude': longitude, 'neighbors': {}}
+            graphStructure[station_id] = {'latitude': latitude, 'longitude': longitude, 'neighbors': {}}
+    
+    G = WeightedGraph(len(graphStructure))
+    for stations in list(graphStructure.keys):
+        G.add_node()
 
     # Read connections data from csv
     with open(connectionsFile, 'r') as connections:
@@ -98,12 +77,21 @@ def main():
         next(connections_reader)  # Skip header
         for row in connections_reader:
             station1, station2 = map(int, row)
-            distance = self.calculate_distance(graph[station1], graph[station2])
-            graph[station1]['neighbors'][station2] = distance
-            graph[station2]['neighbors'][station1] = distance
+            distance = calculate_distance(graphStructure[station1], graphStructure[station2])
+            graphStructure[station1]['neighbors'][station2] = distance
+            graphStructure[station2]['neighbors'][station1] =distance 
+            G.add_edge()
 
-
-        
+def calculate_distance(self, station1, station2):
+    x1, y1 = radians(station1['latitude']), radians(station1['longitude'])
+    x2, y2 = radians(station2['latitude']), radians(station2['longitude'])
+    dlat = x2 - x1
+    dlon = y2 - y1
+    a = sin(dlat / 2) ** 2 + cos(x1) * cos(x2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    earth_radius = 6371.0
+    distance = earth_radius * c
+    return distance
 
     
     
